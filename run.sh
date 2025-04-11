c806c775bfa2080e6ede7fe87a33487b4ffd8962
Khomeini <khomeini.air@gmail.com>
2025-04-12 06:11:42 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export JWT_SECRET=Kk7y+9VXp2b5q8t/w3E6DcF1AhM4JkLmNoPqRsTuVxYz
-export DEEPSEEK_API_KEY=sk-1bf116b3e4a94bc58be22bfb8a9d4f9f
+export JWT_SECRET=[your_jwt_secret]
+export DEEPSEEK_API_KEY=[your_deepseek_api_key]
 mvn spring-boot:run -Dspring-boot.run.arguments=--logging.level.org.springframework=DEBUG
